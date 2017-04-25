4906c3b9c98178cac37d67caf66f19d8711a554e
Evan Briolat <briolate@gmail.com>
2017-04-25 16:43:17 -0400

    Stuff

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+$(document).ready(function){
+
 $(".row").click(function(){
 
   $("popForm").css("display","block");
+});
+
+$('.seat').hover(function(){
+    $(this).fadeTo('fast', '.25');
+  },
+  function(){
+    $(this).fadeTo('fast', '1');
+  }
+);
+
+//document.ready END
 });
