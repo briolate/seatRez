f1048b7efdcdc89d3d6927cf02b011b5f2bdee1a
Evan Briolat <briolate@gmail.com>
2017-04-26 16:07:37 -0400

    Minor changes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
+
 $(".seat").click(function(){
   //or slideToggle depending on what we want
   $(".popForm").slideDown("slow");
-
 });
 //add an if else later? like if the seat is reservered, on hover it will
 //show their info. else fadeTo? butts.
-$('.seat').hover(function(){
 
+//Hover changes opacity
+$('.seat').hover(function(){
     $(this).fadeTo(100, '.35');
   },
   function(){
@@ -16,34 +17,29 @@ $('.seat').hover(function(){
   }
 );
 
-
-// clicked
+//Clicked function to toggle and add class
 $('.seat').click(function(){
+  $(this).toggleClass('seatSelected');
+  $(this).addClass("reserved");
+  });
 
-$(this).toggleClass('seatSelected');
-$(this).addClass("reserved");
-});
-
-
-
-
+//Click function to store reservation
 $('.reserveIt').click(function() {
-      var first = $("#firstName").val();
-      var last = $("#lastName").val();
-      var email = $("#email").val();
-      var data = {
-        // rows: [{
-          First: first,
-          Last: last,
-          Email: email
-        // }]
-      }
+  var first = $("#firstName").val();
+  var last = $("#lastName").val();
+  var email = $("#email").val();
+  var data = {
+    // rows: [{
+      First: first,
+      Last: last,
+      Email: email
+    // }]
+  }
 
 
       console.log(data);
 if($(".seatSelected")){
   $('.people p').text(data.First, data.Last);
-
 }
 });
 
@@ -58,8 +54,6 @@ $('.btn').click(function(){
       $this.text('Show less');
     }
 });
-
-
 
 //document.ready END
 });
